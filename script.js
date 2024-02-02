fff0288745eb1cf0670da4f14e8ca594e2e5eb7b
okankpl <okan149@gmail.com>
2024-02-02 18:49:58 +0100

    added all types and bgr colors

--- script.js
+++ script.js
@@ -1,29 +1,24 @@
 
 let currentPokemon;
-let maxPokedex = 30;
+let maxPokedex = 39;
 let pokemonArray;
 
 async function renderSmallCard() {
     content = document.getElementById('pokedex');
-    content.innerHTML = ''; // Clear the content first
+    content.innerHTML = ''; 
 
     for (let i = 1; i < maxPokedex; i++) {
         let url = 'https://pokeapi.co/api/v2/pokemon/' + i;
         let response = await fetch(url);
-
         if (response.ok) {
             currentPokemon = await response.json();
             let pokemonImage = currentPokemon['sprites']['other']['official-artwork']['front_default'];
             let pokemonType = currentPokemon['types'][0]['type']['name'];
             let name = currentPokemon['name'];
-
             let pokemonContainer = document.createElement('div');
             pokemonContainer.classList.add('pokedex-container');
             addBgrColor(pokemonContainer , pokemonType);
-           
-
-            pokemonContainer.innerHTML = smallCardTemplate(name,pokemonImage,pokemonType);
-            
+            pokemonContainer.innerHTML = smallCardTemplate(name,pokemonImage,pokemonType,i);
             content.appendChild(pokemonContainer);
         } else {
             console.error(`Failed to fetch data for Pokemon ${i}: ${response.statusText}`);
@@ -31,12 +26,12 @@ async function renderSmallCard() {
     }
 }
 
-function smallCardTemplate(name, pokemonImage, pokemonType) {
+function smallCardTemplate(name, pokemonImage, pokemonType, i) {
     return /*HTML*/ `<div>
     <img id="pokemonImage" src="${pokemonImage}" alt="">
 </div>
 <div class="info-container-small-card">
-    <h2 id="pokemonName">${name}</h2>
+    <h2 id="pokemonName">#${i} ${name}</h2>
     <h6 id="type">Type: ${pokemonType}</h6>
 </div>
 `;
@@ -69,5 +64,8 @@ function addBgrColor(pokemonContainer, pokemonType) {
     }
     else if(pokemonType === 'ground') {
         pokemonContainer.classList.add('bgr-color-ground');
+    }
+    else if(pokemonType === 'fairy') {
+        pokemonContainer.classList.add('bgr-color-fairy');
     }
 }
