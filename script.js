ae87948c5bc49b6287ae9f9fa1da48031e22a0b7
okankpl <okan149@gmail.com>
2024-02-02 17:46:50 +0100

    adding render small card function

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
 let currentPokemon;
+let maxPokedex = 12;
+let pokemonArray;
 
 async function loadPokemon() {
     let url ='https://pokeapi.co/api/v2/pokemon/charmander';
@@ -14,4 +16,19 @@ function renderPokemonInfo() {
    name.innerHTML = currentPokemon['name'];
    document.getElementById('pokemonImage').src = currentPokemon['sprites']['other']['official-artwork']['front_default'];
    document.getElementById('type').innerHTML += currentPokemon['types']['0']['type']['name'];
+}
+
+async function renderSmallCard() {
+    content = document.getElementById('pokedex');
+    content = '';
+
+    for (let i = 0; i < maxPokedex; i++) {
+        let url = 'https://pokeapi.co/api/v2/pokemon/' + i;
+        let response = await fetch(url);
+        currentPokemon = await response.json();
+        pokemonArray.push(currentPokemon);
+        let pokemonImage = currentPokemon['sprites']['other']['official-artwork']['front_default'];
+        let pokemonType = currentPokemon['types']['0']['type']['name'];
+
+    }
 }
