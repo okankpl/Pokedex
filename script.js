06b8a727d37a88548730afe18e3757ea9b028b0d
okankpl <okan149@gmail.com>
2024-02-02 17:57:30 +0100

    adding bgr color for each type

--- script.js
+++ script.js
@@ -1,34 +1,39 @@
 
 let currentPokemon;
-let maxPokedex = 12;
+let maxPokedex = 13;
 let pokemonArray;
 
-async function loadPokemon() {
-    let url ='https://pokeapi.co/api/v2/pokemon/charmander';
+for (let i = 0; i < maxPokedex; i++) {
+    let url = 'https://pokeapi.co/api/v2/pokemon/' + i;
     let response = await fetch(url);
-    currentPokemon = await response.json();
-    console.log(currentPokemon);
-    renderPokemonInfo();
-}
 
-function renderPokemonInfo() {
-   let name = document.getElementById('pokemonName');
-   name.innerHTML = currentPokemon['name'];
-   document.getElementById('pokemonImage').src = currentPokemon['sprites']['other']['official-artwork']['front_default'];
-   document.getElementById('type').innerHTML += currentPokemon['types']['0']['type']['name'];
-}
-
-async function renderSmallCard() {
-    content = document.getElementById('pokedex');
-    content = '';
-
-    for (let i = 0; i < maxPokedex; i++) {
-        let url = 'https://pokeapi.co/api/v2/pokemon/' + i;
-        let response = await fetch(url);
+    if (response.ok) {
         currentPokemon = await response.json();
-        pokemonArray.push(currentPokemon);
         let pokemonImage = currentPokemon['sprites']['other']['official-artwork']['front_default'];
-        let pokemonType = currentPokemon['types']['0']['type']['name'];
+        let pokemonType = currentPokemon['types'][0]['type']['name'];
+        let name = currentPokemon['name'];
 
+        // Create a new container for each Pokemon and add a class based on pokemonType
+        let pokemonContainer = document.createElement('div');
+        pokemonContainer.classList.add('pokedex-container');
+
+        // Add a specific class based on pokemonType (for example, 'bgr-color-green' for 'grass' type)
+        if (pokemonType === 'grass') {
+            pokemonContainer.classList.add('bgr-color-green');
+        }
+
+        pokemonContainer.innerHTML = `
+            <div>
+                <img id="pokemonImage" src="${pokemonImage}" alt="">
+            </div>
+            <div class="info-container-small-card">
+                <h2 id="pokemonName">${name}</h2>
+                <h6 id="type">Type: ${pokemonType}</h6>
+            </div>
+        `;
+
+        content.appendChild(pokemonContainer);
+    } else {
+        console.error(`Failed to fetch data for Pokemon ${i}: ${response.statusText}`);
     }
 }
