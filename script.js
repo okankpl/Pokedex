e66fb7d7cad3624b619bd02641fc3c161978f995
okankpl <okan149@gmail.com>
2024-02-03 12:38:51 +0100

    ctyling chart and card

--- script.js
+++ script.js
@@ -109,13 +109,23 @@ for (let j = 0; j < 6; j++) {
         data: {
           labels: stats,
           datasets: [{
-            label: '',
+            label: 'Base Stats',
             data: pokemonStats,
-            borderWidth: 1
-          }]
+            borderWidth: 1,
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'],
+                categoryPercentage: 1, // Anteil der Kategoriebreite, die die Balken einnehmen
+                barPercentage: 1
+            }]
         },
         options: {responsive: true,
             maintainAspectRatio: false,
+           
             indexAxis: 'y',
           scales: {
             y: {
